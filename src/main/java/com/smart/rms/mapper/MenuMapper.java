1250b7e7769e0150721cc1c951865091f8c8cd32
jaeho1987 <jaeho_1987@naver.com>
2025-05-03 19:17:56 +0900

    Initial commit

--- src/main/java/com/smart/rms/mapper/MenuMapper.java
+++ src/main/java/com/smart/rms/mapper/MenuMapper.java
@@ -1,4 +1,8 @@
-package com.smart.rms.model;
+package com.smart.rms.mapper;
+
+import com.smart.rms.model.Menu;
+
+import java.util.List;
 
 public interface MenuMapper {
     List<Menu> selectAllMenus();
