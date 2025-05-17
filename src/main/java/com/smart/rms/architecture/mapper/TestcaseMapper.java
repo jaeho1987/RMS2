4f7f122ad82df3cd0f6ce9b322be4fb91c10e569
Jaeho Lee <jaeho_1987@naver.com>
2025-05-17 19:45:07 +0900

    Merge remote-tracking branch 'origin/fea/jhlee/20250517' into fea/jhlee/20250517

---
+++ src/main/java/com/smart/rms/architecture/mapper/TestcaseMapper.java
@@ -0,0 +1,18 @@
+package com.smart.rms.architecture.mapper;
+
+import com.smart.rms.architecture.model.TbTestcase;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TestcaseMapper {
+    List<TbTestcase> findAll();
+    TbTestcase findById(Long testSeq);
+    List<TbTestcase> findByReqSeq(Long reqSeq);
+    void insert(TbTestcase testcase);
+    int update(TbTestcase testcase);
+    int deleteById(Long testSeq);
+    int updateOrderOne(TbTestcase testcase);
+
+}
